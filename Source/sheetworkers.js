00b3ec5a814dd09e6b1323178f4c60e0b8814920
Steve Radabaugh <steve@Steves-Mac-mini.local>
2020-07-29 12:02:45 -0600

    Duster English Translation

---
+++ Source/sheetworkers.js
@@ -0,0 +1,13 @@
+const sheetVersion = "0.1";
+const sheetName = "Moon Punk";
+const getTranslation = (key) => (getTranslationByKey(key) || "NO_TRANSLATION_FOUND");
+
+//Translate all of the data
+Object.keys(data.playbooks).forEach(playbook =>{
+	const base = data.playbooks[playbook].base;
+	Object.keys(base).forEach
+
+}
+	
+	
+	);
